fa1bc1fd84f7167c89d0a83820eafb862511af5f
Seema-Gupta-88 <Sg8022005@gmail.com>
2024-05-27 23:42:39 +0530

    Add files via upload

---
+++ Question 6.c
@@ -0,0 +1,16 @@
+//WAP to print sum of two numbers.Numbers are taken by user.
+#include<stdio.h>
+int main(){
+	
+	int a;
+	printf("Enter a");
+	scanf("%d",&a);
+ 
+    int b;
+	printf("Enter b");
+	scanf("%d",&b);
+	
+	printf("sum is: %d",a+b);
+	return 0;
+	
+}
